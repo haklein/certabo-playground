85f4df916b813a367345e400d917b1c90b37160f
Harald Klein <hari@vt100.at>
2020-01-01 16:33:26 +0100

    lurking instances should be fixed by instantly breaking in serial thread on EOF

--- certabo-uci/uci.sh
+++ certabo-uci/uci.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 pushd . > /dev/null
 cd ~/src/certabo-playground/certabo-uci
-ps auxw | grep certabo-uci.py | awk '{print $2} ' | while read pid; do kill $pid; done > /dev/null 2>&1 || true
+# ps auxw | grep certabo-uci.py | awk '{print $2} ' | while read pid; do kill $pid; done > /dev/null 2>&1 || true
 python3 certabo-uci.py
 popd > /dev/null
