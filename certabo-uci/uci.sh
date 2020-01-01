26417b8d339dfe6c70faddd97f8b2f17494fb7d7
Harald Klein <hari@vt100.at>
2020-01-01 01:39:23 +0100

    adjust wrapper, we're not using usbtool anymore. Also suppress pushd/popd output as it interferes with UCI specs

--- certabo-uci/uci.sh
+++ certabo-uci/uci.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
-pushd .
+pushd . > /dev/null
 cd ~/src/certabo-playground/certabo-uci
-ps auxw | grep usbtool | awk '{print $2} ' | while read pid; do kill $pid; done > /dev/null 2>&1 || true
-python3 certabo-uci.py --port /dev/ttyUSB0
-popd
+ps auxw | grep certabo-uci.py | awk '{print $2} ' | while read pid; do kill $pid; done > /dev/null 2>&1 || true
+python3 certabo-uci.py
+popd > /dev/null
