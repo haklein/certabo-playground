000233bc8d09421fbc1058e060225060ac0bbf89
Harald Klein <hari@vt100.at>
2020-01-10 23:02:12 +0100

    don't attempt to lock serial port on windows

--- certabo-uci/certabo-uci.py
+++ certabo-uci/certabo-uci.py
@@ -120,8 +120,9 @@ class serialreader(threading.Thread):
                         continue
                     logging.info(f'Opening serial port {serialport}')
                     uart = serial.Serial(serialport, 38400, timeout=2.5)  # 0-COM1, 1-COM2 / speed /
-                    logging.debug(f'Attempting to lock {serialport}')
-                    fcntl.flock(uart.fileno(), fcntl.LOCK_EX | fcntl.LOCK_NB)
+                    if os.name == 'posix':
+                        logging.debug(f'Attempting to lock {serialport}')
+                        fcntl.flock(uart.fileno(), fcntl.LOCK_EX | fcntl.LOCK_NB)
                     logging.debug(f'Flushing input on {serialport}')
                     uart.flushInput()
                     self.connected = True
